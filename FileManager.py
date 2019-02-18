269cd332c0ca1d9bf57f52c0ed8c146c4046414d
Muhammad-Essam-Asem <amirossama123@gmail.com>
2019-02-18 13:31:40 +0300

    Initial commit building class

---
+++ FileManager.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+from os.path import exists
+class FileManager():
+    def __init__(self):
+        print "welcome to file manager..."
+        print "press to copy data file[1]: "
+        print "press to delet the file[2]: "
+        print "perss to change path[3]"
+        print "press to show file data[4]: "
+        print "press to longth of data[5]: "
